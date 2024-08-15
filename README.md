0eab9b6bc9a36909c5e30e7b3838740008338325
Zheng Gao <52664844+ba361006@users.noreply.github.com>
2024-08-15 12:14:12 +0800

    add variables

--- README.md
+++ README.md
@@ -1,22 +1,30 @@
+## To run on local
+- move to react directory
+    ```
+    cd my-react-app
+    ```
+- run localhost
+    ```
+    npm start
+    ```
 
-
-- to add react change to firebase https remote
-    - move to react directory
-        ```
-        cd my-react-app
-        ```
-    - Run the build command to generate the production-ready static files:
-        ```
-        npm run build
-        ```
-    - back to root directory
-        ```
-        cd ..
-        ```
-    - forcely copy the built files to firebase public directory
-        ```
-        cp -r my-react-app/build/* y/
-        ```
+## To deploy on https remote
+- move to react directory
+    ```
+    cd my-react-app
+    ```
+- Run the build command to generate the production-ready static files:
+    ```
+    npm run build
+    ```
+- back to root directory
+    ```
+    cd ..
+    ```
+- forcely copy the built files to firebase public directory
+    ```
+    cp -r -Force my-react-app/build/* y/
+    ```
 - run this command to deploy to remote https firebase
     ```
     firebase deploy
--- my-react-app/src/TelegramLoginWidget.tsx
+++ my-react-app/src/TelegramLoginWidget.tsx
@@ -19,31 +19,39 @@ const TelegramLogin = ()=> {
   const telegramLoginRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    const botUsername = "FoxwowoDevBot";
+    const buttonSize = "large";
+    const cornerRadius = "20";
+    const widgetVersion = "22";
+    const allowBotsendMessageToUser = "write";
+
+
     // Create the script element for the Telegram widget
     const script = document.createElement('script');
     script.async = true;
-    script.src = "https://telegram.org/js/telegram-widget.js?22";
-    script.setAttribute("data-telegram-login", "FoxwowoDevBot");
-    script.setAttribute("data-size", "large");
+    script.src = `https://telegram.org/js/telegram-widget.js?${widgetVersion}`;
+    script.setAttribute("data-telegram-login", botUsername);
+    script.setAttribute("data-size", buttonSize);
+    script.setAttribute("data-radius", cornerRadius);
     script.setAttribute("data-onauth", "onTelegramAuth(user)");
-    script.setAttribute("data-request-access", "write");
+    script.setAttribute("data-request-access", allowBotsendMessageToUser);
 
     if (telegramLoginRef.current) {
-      console.log("append!");
       telegramLoginRef.current?.appendChild(script);
     } else {
-      console.error("Telegram login element not found");
+      console.error("Telegram login widget element not found");
     }
 
     // Define the onTelegramAuth function
     window.onTelegramAuth = function(user) {
+      // id, first_name, last_name, username, photo_url, auth_date and hash fields.
       alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
     };
 
-    // // Cleanup script when component unmounts
-    // return () => {
-    //   document.body.removeChild(script);
-    // };
+    // Cleanup script when component unmounts
+    return () => {
+      telegramLoginRef.current?.removeChild(script);
+    };
   }, []);
 
   return (
